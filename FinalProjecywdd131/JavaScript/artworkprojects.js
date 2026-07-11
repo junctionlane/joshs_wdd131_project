7f580a9a5cc1400ca14cf27a7e31b090c33f544f
Joshbot8 <hoppingamimations105@gmail.com>
2026-07-11 16:01:44 -0700

    List Culling of information

--- FinalProjecywdd131/JavaScript/artworkprojects.js
+++ FinalProjecywdd131/JavaScript/artworkprojects.js
@@ -1,69 +1,32 @@
 ﻿artworkprojects = [
     {
-        author: 'Provo High Culinary Students',
-        url: '',
-        isBasedOn: '',
-        cookTime: '30 Min',
-        datePublished: '2016-10-16',
         tags: ['FN@F', 'Custom Oc', 'Animatronic'],
         description: 'A Fnaf character of my own creation, Most commonly known on the VRC platform',
         image: './FinalProjectImage/image-195.png',
-        recipeIngredient: [
-            
-        ],
         name: 'Glamrock DJ MusicMan',
         prepTime: '2 days',
-        recipeInstructions: [
-            'Add the egg yolks, oil, salt, cayenne, sugar, ginger, shallots, sweet potatoes (steam and mash before), and milk and mix well.',
-            'Next add the cornmeal, chives, and flour and baking powder',
-            'Whip the egg whites until stiff and fold in',
-            'Cook until golden brown in a waffle iron. Serve with butter and Wilted Greens or Honey.'
-        ],
-        recipeYield: '6 waffles',
         rating: 0,
         votes: 0
     },
     {
-        author: 'Provo High Culinary Students',
-        url: '',
-        isBasedOn: '',
-        cookTime: '30 Min',
-        datePublished: '2016-10-16',
         tags: ['StarWars', 'Custom Oc', 'Droid'],
         description: 'ExtraInformation',
         image: './FinalProjectImage/image-9(1).jpg',
-        recipeIngredient: [
-
-        ],
         name: 'Droid',
         prepTime: '5 days',
-        recipeInstructions: [
-            'Add the egg yolks, oil, salt, cayenne, sugar, ginger, shallots, sweet potatoes (steam and mash before), and milk and mix well.',
-            'Next add the cornmeal, chives, and flour and baking powder',
-            'Whip the egg whites until stiff and fold in',
-            'Cook until golden brown in a waffle iron. Serve with butter and Wilted Greens or Honey.'
-        ],
-        recipeYield: '',
         rating: 0,
         votes: 0
-    },{
-        author: "Joshua Lane",
-        category: "Transformers",
-        type: "3D Model",
-        datePublished: "2026-07-01",
+    },
+    {
         tags: ["Transformers", "Dinobot", "Grimlock"],
         description: "3D model of Grimlock in his dinosaur form.",
         image: "./FinalProjectImage/Neon.png",
         model: "./models/Grimlock.glb",
         name: "Grimlock",
         timeSpent: "2 weeks",
-        details: [
-            "Created in Blender.",
-            "Includes textured dinosaur mode.",
-            "Can be viewed and rotated in 3D."
-        ],
         rating: 0,
         votes: 0
-    },]
+    }
+    ]
     
     
