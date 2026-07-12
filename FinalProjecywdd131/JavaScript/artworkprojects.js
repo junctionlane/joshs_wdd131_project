ab95032c6305465a6d656c116d927fb073ea44e6
Joshbot8 <hoppingamimations105@gmail.com>
2026-07-11 18:48:06 -0700

    picturesupdate

--- FinalProjecywdd131/JavaScript/artworkprojects.js
+++ FinalProjecywdd131/JavaScript/artworkprojects.js
@@ -2,7 +2,7 @@
     {
         tags: ['FN@F', 'Custom Oc', 'Animatronic'],
         description: 'A Fnaf character of my own creation, Most commonly known on the VRC platform',
-        image: './FinalProjectImage/image-195.jpg',
+        image: './FinalProjectImage/image-195.webp',
         name: 'Glamrock DJ MusicMan',
         prepTime: '2 days',
         rating: 0,
@@ -11,7 +11,7 @@
     {
         tags: ['StarWars', 'Custom Oc', 'Droid'],
         description: 'ExtraInformation',
-        image: './FinalProjectImage/image-9(1).jpg',
+        image: './FinalProjectImage/image-9(1).webp',
         name: 'Droid',
         prepTime: '5 days',
         rating: 0,
@@ -20,7 +20,7 @@
     {
         tags: ["Transformers", "Dinobot", "Grimlock"],
         description: "3D model of Grimlock in his dinosaur form.",
-        image: "./FinalProjectImage/Neon.png",
+        image: "./FinalProjectImage/Neon.webp",
         model: "./models/Grimlock.glb",
         name: "Grimlock",
         timeSpent: "2 weeks",
