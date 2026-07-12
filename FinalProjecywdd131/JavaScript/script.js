5885035b44ebe304c5b6ac7d489654fcc0a2c484
Joshbot8 <hoppingamimations105@gmail.com>
2026-07-11 19:47:56 -0700

    Update script.js

--- FinalProjecywdd131/JavaScript/script.js
+++ FinalProjecywdd131/JavaScript/script.js
@@ -79,6 +79,7 @@ function artworkTemplate(artwork) {
                 src="${artwork.model}"
                 alt="${artwork.name}"
                 loading="lazy"
+                reveal="manual"
                 camera-controls
                 auto-rotate>
             </model-viewer>
